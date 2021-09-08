f23801f32459b224dfc9906b37d3c76cd4e142ed
Rara <lolyhol@whatever.com>
2021-09-08 17:34:28 +0300

    Made form controlable

--- src/components/BlogContent/components/AddPostForm.js
+++ src/components/BlogContent/components/AddPostForm.js
@@ -1,7 +1,28 @@
 import './AddPostForm.css'
 import CloseIcon from '@material-ui/icons/Close';
+import { Component } from 'react';
 
-export const AddPostForm = ({ handleAddFormHide }) => {
+export class AddPostForm extends Component {
+
+  state = {
+    postTitle: '',
+    postDescription: '',
+  }
+
+  handlePostTitleChange = (e) => {
+    this.setState({
+      postTitle: e.target.value,
+    })
+  }
+
+  handlePostDescription = (e) => {
+    this.setState({
+      postDescription: e.target.value,
+    })
+  }
+
+  render() {
+    const handleAddFormHide = this.props.handleAddFormHide
     return (
       <>
         <form action="" className="addPostForm">
@@ -11,10 +32,20 @@ export const AddPostForm = ({ handleAddFormHide }) => {
           
           <h2>Создать пост</h2>
           <div>
-            <input placeholder="заголовок поста" type="text" name="postTitle" />
+            <input 
+            value={this.state.postTitle}
+            onChange={this.handlePostTitleChange} 
+            placeholder="заголовок поста" 
+            type="text" 
+            name="postTitle" />
           </div>
           <div>
-              <textarea placeholder="описание поста" name="postDesc"></textarea>
+              <textarea 
+              value={this.state.postDescription} 
+              placeholder="описание поста" 
+              name="postDesc"
+              onChange={this.handlePostDescription}
+              ></textarea>
           </div>
           <div>
               <button onClick={handleAddFormHide} className="blackBtn" type="button">Добавить</button>
@@ -23,4 +54,6 @@ export const AddPostForm = ({ handleAddFormHide }) => {
         <div onClick={handleAddFormHide} className="overlay"></div>
       </>
     )
+  }  
+    
 }
